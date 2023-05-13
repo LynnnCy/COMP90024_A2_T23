e48f2831c3e7a22534d098e0b09ec1ef8931ebb0
abrarhayat <abrarhayat@gmail.com>
2023-05-13 19:26:47 +1000

    Fixed errors for HTML warnings for Footer

--- cc-react/src/Components/Footer.js
+++ cc-react/src/Components/Footer.js
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <footer class="py-3 my-4">
-            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                <li class="nav-item"><Link className='nav-link' to="/">Home</Link></li>
-                <li class="nav-item"><Link className='nav-link' to='/today'>Today</Link></li>
-                <li class="nav-item"><Link className='nav-link' to='/map'>Map</Link></li>
+        <footer className="py-3 my-4">
+            <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                <li className="nav-item"><Link className='nav-link' to="/">Home</Link></li>
+                <li className="nav-item"><Link className='nav-link' to='/today'>Today</Link></li>
+                <li className="nav-item"><Link className='nav-link' to='/map'>Map</Link></li>
             </ul>
-            <p class="text-center text-muted">© 2023 Team 23</p>
+            <p className="text-center text-muted">© 2023 Team 23</p>
         </footer>
     );
 }

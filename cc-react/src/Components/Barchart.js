204595a996678195583e26f8a3464e0985d1f7df
abrarhayat <abrarhayat@gmail.com>
2023-05-21 00:26:47 +1000

    Added Mastadon Barchart

--- cc-react/src/Components/Barchart.js
+++ cc-react/src/Components/Barchart.js
@@ -1,32 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Legend, Tooltip } from 'recharts';
 import { processPropertyKey } from '../StringUtil'
-
+import { Row } from "react-bootstrap";
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const ChartComponent = () => {
+    const [barChartSource, setBarChartSource] = useState('twitter')
     const [fetchedData, setFetchedData] = useState(null);
     const [barChartData, setBarChartData] = useState(null)
     useEffect(() => {
-        fetch('http://172.26.130.99:8080/bar_chart_data').then(respData => {
-            respData.json().then(respJson => {
-                setFetchedData(respJson);
-                if (fetchedData === null) {
+        console.log('rendering')
+        let fetchUrl = barChartSource === 'twitter' ? 'http://172.26.130.99:8080/bar_chart_data' : 'http://172.26.130.99:8080/chart_data_mastodon';
+        if (fetchedData === null) {
+            fetch(fetchUrl).then(respData => {
+                respData.json().then(respJson => {
                     setFetchedData(respJson);
-                    let data = []
-                    respJson.series.forEach(curr => {
-                        data.push({ "name": processPropertyKey(curr.name), "amusement": curr.data[0], "awe": curr.data[1], "joy": curr.data[2] })
-                    })
-                    setBarChartData(data)
-                }
+                    if (fetchedData === null) {
+                        setFetchedData(respJson);
+                        let data = []
+                        respJson.series.forEach(curr => {
+                            data.push({ "name": processPropertyKey(curr.name), "amusement": curr.data[0], "awe": curr.data[1], "joy": curr.data[2] })
+                        })
+                        setBarChartData(data)
+                    }
+                })
             })
-        })
-    })
+        }
+    }, [barChartSource, barChartData, fetchedData])
 
     return (
         <div>
             {
                 barChartData !== null
                     ? <>
+                        <Row>
+                            <Dropdown>
+                                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                    Change Data Source
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item onClick={() => {
+                                        setBarChartSource('twitter')
+                                        setFetchedData(null)
+                                    }}>Twitter</Dropdown.Item>
+                                    <Dropdown.Item onClick={() => {
+                                        setBarChartSource('mastadon')
+                                        setFetchedData(null)
+                                    }}>Mastadon</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Row>
                         <h1 className="text-center">{"Emotion Data Statistics"}</h1>
                         <BarChart width={1080} height={650} data={barChartData}>
                             <CartesianGrid strokeDasharray="3 3" />

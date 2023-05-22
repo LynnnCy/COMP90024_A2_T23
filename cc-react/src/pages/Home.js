5ec7849bee7f0cd41b5f9ec05dd5092c4c01d72f
abrarhayat <abrarhayat@gmail.com>
2023-05-22 21:55:30 +1000

    Updated the front page

--- cc-react/src/pages/Home.js
+++ cc-react/src/pages/Home.js
@@ -3,11 +3,16 @@ const Home = () => {
     return (
         <div className='home'>
             <PageTransition>
-                <div className='center' style={{lineHeight: "20rem"}}>
+                <div className='center' style={{
+                    "display": "flex",
+                    "align-items": "center",
+                    "justify-content": "center",
+                    "height": "85vh"
+                }}>
                     <br />
-                    <div style={{color: "blue"}}>
-                        <h1 className='text-center' style={{ color: "white", fontFamily: "impact" }}>Are people really happy?</h1>
-                        <h2 className='text-center' style={{ color: "white", position: 'center', fontFamily: "impact" }}>Let's explore how people share happiness on Social Media</h2>
+                    <div style={{ background: "gray", opacity: 0.8, padding: "2rem" }}>
+                        <h1 className='text-center' style={{ color: "black", fontFamily: "impact" }}>Are people really happy?</h1>
+                        <h2 className='text-center' style={{ color: "black", position: 'center', fontFamily: "impact" }}>We will explore how people share happiness on Social Media</h2>
                     </div>
                 </div>
             </PageTransition>

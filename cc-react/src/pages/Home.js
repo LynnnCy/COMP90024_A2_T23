8594d376c59f6916d4036c1822e5baa45c5c5974
abrarhayat <abrarhayat@gmail.com>
2023-05-23 02:23:05 +1000

    Added a Button to start off from the first page

--- cc-react/src/pages/Home.js
+++ cc-react/src/pages/Home.js
@@ -1,4 +1,6 @@
+import { Button } from 'react-bootstrap';
 import PageTransition from '../Components/PageTransition';
+import { Link } from 'react-router-dom';
 const Home = () => {
     return (
         <div className='home'>
@@ -12,7 +14,10 @@ const Home = () => {
                     <br />
                     <div style={{ background: "gray", opacity: 0.8, padding: "2rem" }}>
                         <h1 className='text-center' style={{ color: "black", fontFamily: "impact" }}>Are people really happy?</h1>
-                        <h2 className='text-center' style={{ color: "black", position: 'center', fontFamily: "impact" }}>We will explore how people share happiness on Social Media</h2>
+                        <h5 className='text-center' style={{ color: "black", position: 'center', fontFamily: "impact" }}>We will conduct a comprehensive analysis to understand the intricate ways in which people express their emotions on various social media platforms.</h5>
+                        <div style={{ display: "flex", "align-items": "center", "justify-content": "center", marginTop: "2rem" }}>
+                            <Link className='nav-link' to="/trending"><Button variant="dark">Explore</Button></Link>
+                        </div>
                     </div>
                 </div>
             </PageTransition>
